f25903273b9a3c3ef20d6aa1c06c04348728787c
Ayon95 <mushfiqurrahman78@yahoo.com>
2021-03-29 00:57:35 -0400

    Added styling to GameIntro component

--- src/components/GameIntro.js
+++ src/components/GameIntro.js
@@ -4,20 +4,26 @@ function GameIntro() {
 	return (
 		<div className="container">
 			<div className="game-intro">
-				<h1 className="game-intro__primary-heading">COVID Aware</h1>
-				<h2 className="game-intro__secondary-heading">A COVID-awareness-building game</h2>
-				<p className="game-intro__description">
-					Test your knowledge of Ontario's COVID-19 restrictions and safety measures. These rules
-					and regulations depend on the zone or level your region is in. There are five zones -
-					green, yellow, orange, red, and grey - with green being the least severe, and grey being
-					the most severe. For more information, you can go to{' '}
-					<a href="https://covid-19.ontario.ca/zones-and-restrictions" className="link">
-						Ontario COVID-19 Zones and Restrictions
-					</a>
-				</p>
-				<Link to="/game" className="btn">
-					Play
-				</Link>
+				<div className="game-intro__header">
+					<h1 className="game-intro__primary-heading">COVID Aware</h1>
+					<h2 className="game-intro__secondary-heading">A COVID-awareness-building game</h2>
+				</div>
+				<div className="game-intro__main">
+					<p className="game-intro__description">
+						Test your knowledge of Ontario's COVID-19 restrictions and safety measures. These rules
+						and regulations depend on the zone or level your region is in. There are five zones -
+						green, yellow, orange, red, and grey - with green being the least severe, and grey being
+						the most severe. For more information, you can go to{' '}
+						<a href="https://covid-19.ontario.ca/zones-and-restrictions" className="link">
+							Ontario COVID-19 Zones and Restrictions
+						</a>
+					</p>
+					<div className="btn-container">
+						<Link to="/game" className="btn">
+							Play
+						</Link>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
